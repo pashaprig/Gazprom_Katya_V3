176872c740edd9a41fd949c9243c3a82319f621d
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2023-05-10 13:41:53 +0300

    Sanction Why desktop

--- source/js/script.js
+++ source/js/script.js
@@ -10,6 +10,8 @@ class App {
     const navMain = document.querySelector('.main-nav');
     const navToggle = document.querySelector('.main-nav__toggle');
     const navButtonText = document.querySelector('.main-nav__open-btn-text');
+    const $sanctions = document.querySelector('.sanctions');
+    const $why = document.querySelector('.why');
 
     const initJS = () => {
       navMain.classList.remove('main-nav--nojs');
@@ -18,11 +20,15 @@ class App {
     const closeOpen = () => {
       navToggle.addEventListener('click', function () {
         if (navMain.classList.contains('main-nav--closed')) {
+          $sanctions.style.display = 'none';
+          $why.classList.add('why--invert-color');
           navMain.classList.remove('main-nav--closed');
           navMain.classList.add('main-nav--opened');
           navButtonText.textContent = 'Закрыть меню';
 
         } else {
+          $sanctions.style.display = 'block';
+          $why.classList.remove('why--invert-color');
           navMain.classList.add('main-nav--closed');
           navMain.classList.remove('main-nav--opened');
           navButtonText.textContent = 'Открыть меню';
